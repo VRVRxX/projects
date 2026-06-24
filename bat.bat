99db4d3687c44604f75700a7bfdafe09a398da6a
VRVRxX <voldemaars.lvovs@gmail.com>
2026-06-24 19:59:59 +0300

    Update bat.bat

--- bat.bat
+++ bat.bat
@@ -1,2 +1,2 @@
 @echo off
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Windows\System32\wallpaper.jpg" /f & RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\wallpaper.jpg" /f & RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
