1778177a7492bd23ab1b3d553fdff8e49db2641c
VRVRxX <voldemaars.lvovs@gmail.com>
2026-09-20 13:20:42 +0300

    Update sdestr.py

--- sdestr.py
+++ sdestr.py
@@ -1,7 +1,7 @@
 import time
 import requests
 
-MESSAGE = "Your preset message here"
+MESSAGE = ".selfdestruct"
 
 WEBHOOKS = [
     "https://discord.com/api/webhooks/1551164982593462282/srpFQ2AvOGBRFi_YwkbM38K42s8DdveLwHqg4Lj5sHwZawAtt6sqpYc6zP85SJ-8S7MB?thread_id=1478772480867958825",
