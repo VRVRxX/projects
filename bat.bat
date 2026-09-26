9292dbb550b0ba3c64c31671fe2e91c5134a4e1c
VRVRxX <voldemaars.lvovs@gmail.com>
2026-09-26 19:33:44 +0300

    Delete bat.bat

--- bat.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\wallpaper.jpg" /f & RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
